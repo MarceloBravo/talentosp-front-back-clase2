d4c03505c69d9b2d1d5d78bc47fdf862f9d88bfc
Marcelo Bravo <mabc@live.cl>
2025-12-22 15:11:19 -0300

    fix: Se implementó mantenedor de usuarios: 1.-Se creó el formulario del mantenedor de usuarios en el directorio auth-frontend\src\pages\Users\FormUsersPage 2.-Se desintaló toastify por incompatibilidad con React 19, en su lugar se implementarios alertas y mensajes de window

--- auth-frontend/src/pages/Users/FormUsersPage/FormUsersPage.jsx
+++ auth-frontend/src/pages/Users/FormUsersPage/FormUsersPage.jsx
@@ -1,7 +1,173 @@
-import './FormUsersPage.module.css'
+import MenuComponent from '../../../componentes/Menu/MenuComponent';
+import { SpinnerComponent } from '../../../componentes/Spinner/SpinnerComponent';
+import styles from './FormUsersPage.module.css';
+import { useFormUsersPage } from './useFormUsersPage';
 
 export const FormUsersPage = () => {
+  const {
+    id,
+    NUEVO,
+    loading,
+    form,
+    formErrors,
+    handleInputChange,
+    handleSubmit,
+    handleBtnEliminar,
+    handleBtnCancelar
+  } = useFormUsersPage();
+
+
+
   return (
-    <div>UsersPage</div>
+    <>
+      {loading && <SpinnerComponent/>}
+      <MenuComponent />
+      <div>
+        <h2>Mantenedor de usuarios</h2>
+        <form className={styles.formUsuarios} onSubmit={handleSubmit}>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="username">Nombre de usuario</label>
+                <div className={styles.fieldContainer}>
+                  <input 
+                    type="text"
+                    className={styles.inputField} 
+                    id='username' 
+                    name='username' 
+                    placeholder='Nombre de usuario' 
+                    maxLength="20"
+                    required
+                    value={form.username}
+                    onChange={e => handleInputChange(e)}
+                    >
+                  </input>
+                  {formErrors.username && <span className='label-error'>{formErrors.username}</span>}
+                </div>
+              </div>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="nombre">Nombre</label>
+                <div className={styles.fieldContainer}>
+                  <input 
+                    type="text"
+                    className={styles.inputField} 
+                    id='nombre' 
+                    name='nombre' 
+                    placeholder='Nombre' 
+                    maxLength="20"
+                    required
+                    value={form.nombre}
+                    onChange={e => handleInputChange(e)}
+                    >
+                  </input>
+                  {formErrors.nombre && <span className='label-error'>{formErrors.nombre}</span>}
+                </div>
+              </div>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="apellido">Apellido</label>
+                <div className={styles.fieldContainer}>
+                  <input 
+                    type="text"
+                    className={styles.inputField} 
+                    id='apellido' 
+                    name='apellido' 
+                    placeholder='Apellido' 
+                    maxLength="20"
+                    required
+                    value={form.apellido}
+                    onChange={e => handleInputChange(e)}
+                    >
+                  </input>
+                  {formErrors.apellido && <span className='label-error'>{formErrors.apellido}</span>}
+                </div>
+              </div>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="email">Email</label>
+                <div className={styles.fieldContainer}>
+                  <input 
+                    type="email"
+                    className={styles.inputField} 
+                    id='email' 
+                    name='email' 
+                    placeholder='Nombre de usuario' 
+                    maxLength="255"
+                    required
+                    value={form.email}
+                    onChange={e => handleInputChange(e)}
+                    >
+                  </input>
+                  {formErrors.email && <span className='label-error'>{formErrors.email}</span>}
+                </div>
+              </div>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="rol">Rol</label>
+                <div className={styles.fieldContainer}>
+                  <select  
+                    className={styles.inputField} 
+                    id='rol' 
+                    name='rol' 
+                    required
+                    value={form.rol}
+                    onChange={e => handleInputChange(e)}
+                    >
+                      <option value="" disabled>Seleccione un rol</option>
+                      <option value="admin">admin</option>
+                      <option value="user">administrativo</option>
+                      <option value="guest">invitado</option>
+                  </select>
+                  {formErrors.rol && <span className='label-error'>{formErrors.rol}</span>}
+                </div>
+              </div>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="password">Contraseña</label>
+                <div className={styles.fieldContainer}>
+                  <input 
+                    type="password"
+                    className={styles.inputField} 
+                    id='password' 
+                    name='password' 
+                    placeholder='Contraseña' 
+                    maxLength="20"
+                    required={id === '' || id === NUEVO}
+                    value={form.password}
+                    onChange={e => handleInputChange(e)}
+                    >
+                  </input>
+                  {formErrors.password && <span className='label-error'>{formErrors.password}</span>}
+                </div>
+              </div>
+            
+            <div className={styles.formGroup}>
+                <label htmlFor="confirm_password">Confirmar contraseña</label>
+                <div className={styles.fieldContainer}>
+                  <input 
+                    type="password"
+                    className={styles.inputField} 
+                    id='confirm_password' 
+                    name='confirm_password' 
+                    placeholder='Confirmación de contraseña' 
+                    maxLength="20"
+                    required={id === '' || id === NUEVO}
+                    value={form.confirm_password}
+                    onChange={e => handleInputChange(e)}
+                    >
+                  </input>
+                  {formErrors.confirm_password && <span className='label-error'>{formErrors.confirm_password}</span>}
+                </div>
+    
+              </div>
+              <hr/>
+              <div className={styles.buttonContent}>
+                <button type="submit" className={styles.btnSuccess}>Grabar</button>
+                <button type="button" className={styles.btnDelete + ` ${id === '' || id === NUEVO ? styles.btnDisabled : ''}`} onClick={handleBtnEliminar} disabled={id === '' || id === NUEVO}>Eliminar</button>
+                <button type="button" className={styles.btnCancel} onClick={handleBtnCancelar}>Salir</button>
+              </div>
+        </form>
+      </div>
+    </>
   )
 }
--- auth-frontend/src/pages/Users/FormUsersPage/useFormUsersPage.jsx
+++ auth-frontend/src/pages/Users/FormUsersPage/useFormUsersPage.jsx
@@ -1,7 +1,181 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useHttp } from '../../../hooks/useHttp';
 
 export const useFormUsersPage = () => {
-  return (
-    <div>useFormUsersPage</div>
-  )
+  const { loading, error, data, request } = useHttp();
+  const params = useParams();
+  const id = params.id ?? '';
+  const navigate = useNavigate();
+  const [ form, setForm ] = useState({
+    username: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: '',
+    confirm_password: ''
+  });
+  const [ formErrors, setFormErrors ] = useState({
+    username: '',
+    nombre: '',
+    apellido: '',
+    email: '',
+    rol: '',
+    password: '',
+    confirm_password: ''
+  }); 
+  const NUEVO = 'nuevo';
+
+
+  useEffect(()=> {
+    if(id.trim().length > 0 && id !== NUEVO){
+      const buscarUsuario = async ()=> {
+        try{
+          const response = await request('/api/users/'+id)
+          if(response.data.length === 0) return navigate('/usuarios');
+          delete response.data[0].created_at;
+          response.data[0].password = ''
+          response.data[0].confirm_password = ''
+          setForm(response.data[0])
+        }catch(error){
+          alert(error.message)
+        }
+      }
+
+      buscarUsuario();
+    }
+    // eslint-disable-next-line
+  },[id])
+
+ 
+
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    if(value.trim().length === 0){
+        if(
+          
+          ( (id === '' || id === NUEVO) && ['password', 'confirm_password'].includes(name) ) || 
+          !['password', 'confirm_password'].includes(name)
+        ){
+            setFormErrors({...formErrors, [name]: 'El campo no puede estar vacío'});
+        }else{
+          setFormErrors({...formErrors, [name]: ''});
+        }
+
+        }else if(name === 'password') {
+            if(
+              (value === form.confirm_password && form.confirm_password.trim().length > 0) || 
+              ((id === '' || id === NUEVO) && ['password', 'confirm_password'].includes(name) && value.length === 0)
+            ){
+                setFormErrors({...formErrors, password: '', confirm_password: ''})
+            }else{
+                setFormErrors({...formErrors, [name]: value !== form.confirm_password ?'La contraseña y la confirmación de contraseña no son iguales' : ''})       
+            }
+            
+        }else if(name === 'confirm_password'){
+            if(value === form.password && form.password.trim().length > 0){
+                setFormErrors({...formErrors, password: '', confirm_password: ''})
+            }else{
+                setFormErrors({...formErrors, [name]: value !== form.password ? 'La contraseña y la confirmación de contraseña no son iguales' : ''})
+            }
+        }else{
+            setFormErrors({...formErrors, [name]: ''});
+        }
+
+        setForm({...form,[name]: value })
+  }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if(!validaDatos()){
+      alert('Datos incompletos o no válidos');
+      return;
+    }
+    try{
+      if(id === '' || id === NUEVO){
+        await crearNuevoUsuario();
+      }else{
+        await actualizarUsuario();
+      }
+      navigate('/usuarios')
+    }catch(error){
+      alert(error.message)
+    }
+  }
+  
+  const crearNuevoUsuario = async () => {
+    await request('/api/users', 'POST', form)
+    alert('Usuario creado correctamente')
+  }
+
+  const actualizarUsuario = async () => {
+    await request('/api/users/' + id, 'PUT', form)
+    alert('Usuario actualizado correctamente')
+  }
+
+  const handleBtnEliminar = async () => {
+    const resp = window.confirm("¿Desea eliminar el usuario");
+    if(resp){
+      try{
+        await request('/api/users/' + id, 'DELETE')
+        alert('Usuario eliminado exitosamente')
+        navigate('/usuarios')        
+      }catch(error){
+        alert(error.message);
+      }
+    }
+  }
+
+
+  const handleBtnCancelar = () => {
+    navigate('/usuarios')
+  }
+
+
+  const validaDatos = () => {
+    if(
+      form.username.trim().length === 0 ||
+      form.username.length > 20 || 
+      form.nombre.trim().length === 0 || 
+      form.nombre.length > 20 ||
+      form.apellido.trim().length === 0 || 
+      form.apellido.length > 20 ||
+      form.email.trim().length === 0 ||
+      form.email.length > 255 ||
+      form.rol === ''
+    ){
+      return false;
+    }
+
+    if(
+      (id ==='' || id === NUEVO) && 
+      (
+        form.password.length === 0 || 
+        form.password.length > 20 || 
+        form.confirm_password.length === 0 || 
+        form.confirm_password.length > 20
+      )
+    ){
+      return false;
+    }
+    return true;
+  }
+
+  return {
+    id,
+    NUEVO,
+    loading,
+    error,
+    data,
+    form,
+    formErrors,
+    handleInputChange,
+    handleSubmit,
+    handleBtnEliminar,
+    handleBtnCancelar
+  }
 }
